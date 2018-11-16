d4db8bef42d53ff67578b71b3b12856e8a157f35
Robert Lee <bobbydwise1@hotmail.com>
2018-11-16 14:38:27 -0800

    redo anagram logic successful

--- lib/anagrams_antigrams_case.rb
+++ lib/anagrams_antigrams_case.rb
@@ -6,24 +6,78 @@ class Anagrams_Words
     @user_second_word = user_second_word
   end
 
-  def check_reverse_match()
-    if self.user_input_word.reverse.downcase == self.user_second_word.downcase
+  def count_letters(string)
+    index = 0
+    letter_array_count = Array.new(26, 0)
+    while index < string.length
+      case string[index]
+      when "a"
+        letter_array_count[0] = letter_array_count[0] + 1
+      when "b"
+        letter_array_count[1] = letter_array_count[1] + 1
+      when "c"
+        letter_array_count[2] = letter_array_count[2] + 1
+      when "d"
+        letter_array_count[3] = letter_array_count[3] + 1
+      when "e"
+        letter_array_count[4] = letter_array_count[4] + 1
+      when "f"
+        letter_array_count[5] = letter_array_count[5] + 1
+      when "g"
+        letter_array_count[6] = letter_array_count[6] + 1
+      when "h"
+        letter_array_count[7] = letter_array_count[7] + 1
+      when "i"
+        letter_array_count[8] = letter_array_count[8] + 1
+      when "j"
+        letter_array_count[9] = letter_array_count[9] + 1
+      when "k"
+        letter_array_count[10] = letter_array_count[10] + 1
+      when "l"
+        letter_array_count[11] = letter_array_count[11] + 1
+      when "m"
+        letter_array_count[12] = letter_array_count[12] + 1
+      when "n"
+        letter_array_count[13] = letter_array_count[13] + 1
+      when "o"
+        letter_array_count[14] = letter_array_count[14] + 1
+      when "p"
+        letter_array_count[15] = letter_array_count[15] + 1
+      when "q"
+        letter_array_count[16] = letter_array_count[16] + 1
+      when "r"
+        letter_array_count[17] = letter_array_count[17] + 1
+      when "s"
+        letter_array_count[18] = letter_array_count[18] + 1
+      when "t"
+        letter_array_count[19] = letter_array_count[19] + 1
+      when "u"
+        letter_array_count[20] = letter_array_count[20] + 1
+      when "v"
+        letter_array_count[21] = letter_array_count[21] + 1
+      when "w"
+        letter_array_count[22] = letter_array_count[22] + 1
+      when "x"
+        letter_array_count[23] = letter_array_count[23] + 1
+      when "y"
+        letter_array_count[24] = letter_array_count[24] + 1
+      when "z"
+        letter_array_count[25] = letter_array_count[25] + 1
+      else
+        puts "error in score tracking"
+      end
+    index = index + 1
+    end
+    return letter_array_count
+  end
+
+  def check_if_anagram()
+    if count_letters(self.user_input_word.downcase) == count_letters(self.user_second_word.downcase)
       return true
     else
       return false
     end
   end
-
-  def count_letters(string)
-    index = 0
-    alphabet = ('a'..'z').to_a
-    letter_array_count = Array.new(26, 0)
-    while index < string.length
-      if string[index].ord = alphabet[string[index].ord-97]
-      index = index + 1
-    end
-
-end
 
   def check_for_vowels?()
     return self.user_second_word.downcase().match(/[aeiou]/) != nil
@@ -37,4 +91,5 @@ end
   def change_phrase_into_wordarray()
     output_array = user_input_word.downcase.scan(/\w+/)
   end
+
 end
--- spec/anagrams_antigrams_case_spec.rb
+++ spec/anagrams_antigrams_case_spec.rb
@@ -8,24 +8,24 @@ describe('#anagram_antigrams_case_spec tester') do
   #Testing case1: check if two words are simple direct reversals of each other
   it('compares two words if they are anagrams') do
     test_object1 = Anagrams_Words.new("ohio","oiho")
-    expect(test_object1.check_reverse_match()).to(eq(true))
+    expect(test_object1.check_if_anagram()).to(eq(true))
   end
   it('compares two words if they are anagrams') do
     test_object2 = Anagrams_Words.new("ohio","notamatch")
-    expect(test_object2.check_reverse_match()).to(eq(false))
+    expect(test_object2.check_if_anagram()).to(eq(false))
   end
   #Testing case2: ignore case and check if two words are simple direct reversals of each other
   it('compares two words if they are anagrams & ignores uppercase') do
     test_object3 = Anagrams_Words.new("Ohio","oihO")
-    expect(test_object3.check_reverse_match()).to(eq(true))
+    expect(test_object3.check_if_anagram()).to(eq(true))
   end
   it('compares two words if they are anagrams & ignores uppercase') do
     test_object4 = Anagrams_Words.new("Ohio","oiho")
-    expect(test_object4.check_reverse_match()).to(eq(true))
+    expect(test_object4.check_if_anagram()).to(eq(true))
   end
   it('compares two words if they are anagrams & ignores uppercase') do
     test_object5 = Anagrams_Words.new("ohio","oihO")
-    expect(test_object5.check_reverse_match()).to(eq(true))
+    expect(test_object5.check_if_anagram()).to(eq(true))
   end
   #Testing case3: check if the second word has vowels
   it('checks if the second word has vowels') do
@@ -46,5 +46,11 @@ describe('#anagram_antigrams_case_spec tester') do
     test_object9 = Anagrams_Words.new("Hello there!  Alls.","test")
     expect(test_object9.change_phrase_into_wordarray()).to(eq(["hello","there","alls"]))
   end
+
+  it('counts letters in a word') do
+    test_object10 = Anagrams_Words.new("Hello","test")
+    expect(test_object10.count_letters("what")).to(eq([1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0]))
+  end
+
 
 end
