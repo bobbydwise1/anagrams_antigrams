052532791e02367b2f980ef8cd2ef6587ea6fa2c
Robert Lee <bobbydwise1@hotmail.com>
2018-11-16 09:30:23 -0800

    Add first working test for reverse string check

--- lib/anagrams_antigrams_case.rb
+++ lib/anagrams_antigrams_case.rb
@@ -1,14 +1,15 @@
 class Anagrams_Words
-  attr_accessor(:user_input_word)
+  attr_accessor(:user_input_word,:user_second_word)
 
   Anagrams_Words < String
 
-  def initialize(user_input_word)
+  def initialize(user_input_word,user_second_word)
     @user_input_word = user_input_word
+    @user_second_word = user_second_word
   end
 
-  def check_reverse_match(user_second_word)
-    if self.user_input_word.revese() == user_second_word
+  def check_reverse_match()
+    if self.user_input_word.reverse() == self.user_second_word
       return true
     else return false
     end
--- spec/anagrams_antigrams_case_spec.rb
+++ spec/anagrams_antigrams_case_spec.rb
@@ -2,10 +2,11 @@ require('rspec')
 require('pry')
 require('anagrams_antigrams_case')
 
+test_object = Anagrams_Words.new("ohio","oiho")
 
 describe('#check_reverse_match') do
   it('compares users word to second word if they are anagrams') do
-    expect("ohio".check_reverse_match("oiho")).to(eq(true))
+    expect(test_object.check_reverse_match()).to(eq(true))
   end
 
 end
